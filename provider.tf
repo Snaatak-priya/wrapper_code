9c81d138f9ff3e59a41697360a5f1633e01b6859
Snaatak-priya <142471903+Snaatak-priya@users.noreply.github.com>
2023-10-18 18:18:02 +0530

    Update provider.tf
    Removing terraform block of S3 bucket & dynamodb table

--- provider.tf
+++ provider.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket                  = "snaatak-project-tfstate-storage"
-    key                     = "snaatak-project"
-    encrypt                 = true 
-    region                  = "ap-south-1"
-    dynamodb_table          = "snaatak-project-lock-table"
-  }
-}
