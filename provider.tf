73db832fc7ab381d9a378342bc70e1631bcd9223
Snaatak-priya <142471903+Snaatak-priya@users.noreply.github.com>
2023-10-18 18:13:26 +0530

    Update provider.tf
    adding backend.tf file configurations

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket                  = var.s3_bucket_name
+    key                     = "snaatak-project"
+    encrypt                 = true 
+    region                  = "ap-south-1"
+    dynamodb_table          = var.dynamodb_table_name
+  }
+}
