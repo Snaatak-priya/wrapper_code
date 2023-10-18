ad957d2e166ac4fb02c988ef0b8159e6cc983791
Snaatak-priya <142471903+Snaatak-priya@users.noreply.github.com>
2023-10-18 18:28:21 +0530

    Update main.tf
    removing vpc section from dev branch as well, after removing vpc section from main branch

--- main.tf
+++ main.tf
@@ -18,10 +18,4 @@ source = "git::https://github.com/Snaatak-priya/terraform_modules.git//backendMo
 
 }
 
-module "network_skeleton" {
-source = "git::https://github.com/Snaatak-priya/terraform_modules.git//network_skeleton?ref=main"
-  
-  vpc_cidr        = var.vpc_cidr
-  vpc_name        = var.vpc_name
 
-}
